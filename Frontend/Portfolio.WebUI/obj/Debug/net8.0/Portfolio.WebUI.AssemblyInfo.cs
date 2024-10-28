bb2175b7c0c32be1e2e3a82aea4c6080aac6d06e
onuraydi <onuraydi@outlook.com>
2024-10-28 13:41:48 +0300

    ...

--- Frontend/Portfolio.WebUI/obj/Debug/net8.0/Portfolio.WebUI.AssemblyInfo.cs
+++ Frontend/Portfolio.WebUI/obj/Debug/net8.0/Portfolio.WebUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portfolio.WebUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dcef6a0de1cfe346354433119241f8d4e5637339")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f952b54316e8e4c4edb7215169cc2aa67208bff9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portfolio.WebUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portfolio.WebUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
